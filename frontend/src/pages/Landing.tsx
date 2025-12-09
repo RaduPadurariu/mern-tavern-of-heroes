f81764fd6e1bede5fa0366a00059d007c6f08652
RaduPadurariu <padurariuionutradu@gmail.com>
2025-12-09 12:31:44 +0200

    Implement Landing Page and Footer

---
+++ frontend/src/pages/Landing.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router";
+
+const Landing = () => {
+  return (
+    <section className="flex items-center justify-center text-center text-(--light-color) w-full">
+      <div className="flex flex-col items-center justify-center">
+        <Link
+          to="/login"
+          className="text-(--light-color) bg-(--primary-color) mb-4 md:mb-8 inline-block px-2 py-1.5 md:py-3.5 md:px-5 font-bold rounded-[10px] cursor-pointer w-[150px] md:w-[200px] text-xl md:text-3xl  outline-0 border-2 border-(--light-color) hover:text-(--primary-color) hover:bg-(--light-color) transition-all duration-300 ease-in-out"
+        >
+          Login
+        </Link>
+        <Link
+          to="/register"
+          className="text-(--light-color) bg-(--primary-color)  inline-block px-2 py-1.5 md:py-3.5 md:px-5 font-bold rounded-[10px] cursor-pointer w-[150px] md:w-[200px] text-xl md:text-3xl  outline-0 border-2 border-(--light-color) hover:text-(--primary-color) hover:bg-(--light-color) transition-all duration-300 ease-in-out"
+        >
+          Sign Up
+        </Link>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,21 +1,23 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "./layouts/RootLayout";
-import Home from "./pages/Home";
-import Heroes from "./pages/Heroes";
+import Account from "./pages/Account";
 import Posts from "./pages/Posts";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
+import Heroes from "./pages/Heroes";
+import Landing from "./pages/Landing";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
     children: [
-      { index: true, element: <Home /> },
+      { index: true, element: <Landing /> },
       { path: "heroes", element: <Heroes /> },
       { path: "login", element: <LogIn /> },
       { path: "register", element: <SignUp /> },
       { path: "posts", element: <Posts /> },
+      { path: "account", element: <Account /> },
     ],
   },
 ]);
